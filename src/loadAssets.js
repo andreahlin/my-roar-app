5797acd4d5e81b440410d5e434b85ed3d5b16f77
andreahlin <andrea.lin96@yahoo.com>
2023-11-08 15:10:47 -0700

    Add cat images for block 2

--- src/loadAssets.js
+++ src/loadAssets.js
@@ -1,5 +1,14 @@
 import jsPsychPreload from '@jspsych/plugin-preload';
 
+import cat1 from './assets/cat/1.jpg';
+import cat2 from './assets/cat/2.jpg';
+import cat3 from './assets/cat/3.jpg';
+import cat4 from './assets/cat/4.jpg';
+import cat5 from './assets/cat/5.jpg';
+
+// Reference these files in a new array
+const catImages = [cat1, cat2, cat3, cat4, cat5];
+
 // Create arrays of hot dog / not hot dog images
 const numFiles = 5;
 const hotDogFiles = Array.from(Array(numFiles), (_, i) => i + 1).map(
@@ -21,3 +30,9 @@ export const preloadImages = {
   type: jsPsychPreload,
   images: allFiles,
 };
+
+// Preload the cat image
+export const preloadCatImages = {
+  type: jsPsychPreload,
+  images: catImages,
+}
